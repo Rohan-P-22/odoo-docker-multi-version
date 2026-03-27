949cc34dfdea1c8da7189470cf2870d78bf65333
Rohan-P-22 <rohanprajapati200322@gmail.com>
2026-03-27 16:25:18 +0530

    improve deploy strategy

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ docker pull rohanp1722/odoo-multi-version:latest
 
 echo "🔄 Restart Odoo18..."
 cd /opt/odoo-docker/odoo18
-docker compose down
-docker compose up -d
+docker compose pull
+docker compose up -d --build
 
 echo "🔄 Restart Odoo19..."
 cd /opt/odoo-docker/odoo19
-docker compose down
-docker compose up -d
+docker compose pull
+docker compose up -d --build
 
 echo "🔥 NEW DEPLOY WORKING 🔥"
 echo "✅ Deployment complete!"
